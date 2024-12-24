46e7e5c797bc75434d27cd84672e2a0ffe8d2b95
husseinx1970 <husseinmormor@gmail.com>
2024-12-25 00:28:56 +0100

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -1,1 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanera och hämta data från formuläret
+    $namn = htmlspecialchars($_POST['namn']);
+    $telefon = htmlspecialchars($_POST['telefon']);
+    $email = htmlspecialchars($_POST['email']);
+    $beskrivning = htmlspecialchars($_POST['beskrivning']);
 
+    // E-postinställningar
+    $to = "info@paketfart.se"; // Din mottagaradress
+    $subject = "Ny bokning från $namn";
+    $message = "
+    Namn: $namn\n
+    Telefon: $telefon\n
+    E-post: $email\n
+    Beskrivning: $beskrivning
+    ";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Skicka e-post
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Ditt meddelande har skickats!";
+    } else {
+        echo "Tyvärr, något gick fel. Försök igen.";
+    }
+}
+?>
