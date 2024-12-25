2cc8b764bcd7878cd1deca71aba74e4ba38f7fb8
husseinx1970 <husseinmormor@gmail.com>
2024-12-25 01:57:06 +0100

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -10,7 +10,7 @@ $mail->isSMTP();
 $mail->Host = 'mail.paketfarten.se'; // SMTP-servern för paketfarten.se (detta kan variera, kolla med din e-postleverantör)
 $mail->SMTPAuth = true;
 $mail->Username = 'info@paketfarten.se'; // Din e-postadress
-$mail->Password = 'ditt-losenord'; // E-postens lösenord eller app-lösenord
+$mail->Password = 'rmoa naxc hlkf lhbk'; // E-postens lösenord eller app-lösenord
 $mail->SMTPSecure = 'tls'; // 'ssl' eller 'tls', beroende på serverinställningar
 $mail->Port = 587; // Använd rätt port (587 för TLS, 465 för SSL)
 
