be04f5d1576e5601c3f460c4f6a2b47f2de77a93
husseinx1970 <husseinmormor@gmail.com>
2024-12-25 02:29:29 +0100

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -1,29 +1,39 @@
 <?php
-// Inkludera PHPMailer
-require 'PHPMailer/PHPMailerAutoload.php'; // Ange rätt sökväg om du har placerat PHPMailer i en annan mapp
+// Inkludera PHPMailer-klasserna via namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// Skapa instans av PHPMailer
-$mail = new PHPMailer;
+// Inkludera PHPMailer-filerna
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
-// Konfigurera SMTP
-$mail->isSMTP();
-$mail->Host = 'mail.paketfarten.se'; // SMTP-servern för paketfarten.se (detta kan variera, kolla med din e-postleverantör)
-$mail->SMTPAuth = true;
-$mail->Username = 'info@paketfarten.se'; // Din e-postadress
-$mail->Password = 'rmoa naxc hlkf lhbk'; // E-postens lösenord eller app-lösenord
-$mail->SMTPSecure = 'tls'; // 'ssl' eller 'tls', beroende på serverinställningar
-$mail->Port = 587; // Använd rätt port (587 för TLS, 465 för SSL)
+// Skapa ett PHPMailer-objekt
+$mail = new PHPMailer(true);  // Skapar ett PHPMailer-objekt med Exception-stöd
 
-// Konfigurera e-postinställningar
-$mail->setFrom('info@paketfarten.se', 'Paketfart'); // Avsändaradress och namn
-$mail->addAddress('mottagare@exempel.com', 'Mottagare'); // Mottagarens e-postadress
-$mail->Subject = 'Test e-post från Paketfart';
-$mail->Body    = 'Det här är ett testmeddelande från Paketfart-formuläret.';
+try {
+    // Serverinställningar
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';  // Använd din SMTP-server
+    $mail->SMTPAuth = true;
+    $mail->Username = 'din.email@gmail.com';  // Din e-postadress
+    $mail->Password = 'ditt_lösenord';  // Ditt lösenord eller appspecifikt lösenord
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
 
-// Skicka e-post
-if($mail->send()) {
-    echo 'Meddelandet har skickats!';
-} else {
-    echo 'Fel: ' . $mail->ErrorInfo;
+    // Mottagare och avsändare
+    $mail->setFrom('din.email@gmail.com', 'Paketfart');  // Avsändare
+    $mail->addAddress('info@paketfarten.se', 'Mottagare');  // Mottagare
+
+    // Innehåll
+    $mail->isHTML(true);  // Skicka som HTML
+    $mail->Subject = 'Ny bokning från ' . $_POST['namn'];
+    $mail->Body    = 'Namn: ' . $_POST['namn'] . '<br>Telefon: ' . $_POST['telefon'] . '<br>E-post: ' . $_POST['email'] . '<br><br>Beskrivning:<br>' . $_POST['beskrivning'];
+
+    // Skicka e-post
+    $mail->send();
+    echo 'Bokningen skickades!';
+} catch (Exception $e) {
+    echo "Något gick fel: {$mail->ErrorInfo}";
 }
 ?>
